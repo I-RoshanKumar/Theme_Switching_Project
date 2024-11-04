bbc409dbf4e672092c8c4eae814e0f2005fef3e4
Roshan Kunmar <anitamishraasansol@gmail.com>
2024-11-04 19:57:33 +0530

    issues fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,36 @@ import "./App.css";
 import Card from "./Components/Card";
 import ThemeBtn from "./Components/ThemeBtn";
 import { ThemeProvider } from "./Context/theme";
-import { useEffect,useState } from 'react';
+import { useEffect, useState } from "react";
 
 function App() {
-  const [themeMode,sethemeMode] =useState("light")
+  const [themeMode, sethemeMode] = useState("light");
 
   const lightTheme = () => {
-    sethemeMode("light")
-  }
-  
+    sethemeMode("light");
+  };
+
   const darkTheme = () => {
-    sethemeMode("dark")
-  }
+    sethemeMode("dark");
+  };
 
   //actual cahnge in theme mode
   useEffect(() => {
-  document.querySelector('html').classList.remove("light","dark")
-  document.querySelector('html').classList.add(themeMode)
-},[themeMode])
+    document.querySelector("html").classList.remove("light", "dark");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
 
   return (
-    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
       <div className="flex flex-wrap min-h-screen items-center">
         <div className="w-full">
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
 
-          <div className="w-full max-w-sm mx-auto"><Card/></div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
